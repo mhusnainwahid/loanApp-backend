20c7cf0bcfdf60e1f1c9ba9ab6caf4357d170443
mhusnainwahid <mhusnainwahid@gmail.com>
2025-08-04 19:56:58 +0500

    done

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import connectMongoDb from "./config/mongodb.js";
 import authRoutes from "./routes/auth.js";
 import dotenv from "dotenv"
+import cors from 'cors'
 
 const app = express()
 dotenv.config()
+app.use(cors())
 
 const PORT = process.env.PORT || 5000
 connectMongoDb()
+
 app.use(express.json())
 app.use('/',authRoutes)
 
